0af6bb804b9a86e585926e242211f995883f7dd3
Isaac <faceless.shop@gmail.com>
2012-01-07 14:22:40 -0700

    Initial commit

---
+++ reg_replace.py
@@ -0,0 +1,63 @@
+import sublime
+import sublime_plugin
+
+rrsettings = hv_settings = sublime.load_settings('reg_replace.sublime-settings')
+
+
+class RegReplaceMenuCommand(sublime_plugin.WindowCommand):
+    def replace(self, value):
+        if value != -1:
+            command = self.names[value]
+            self.window.run_command("reg_replace", {'reg_command': command})
+
+    def run(self):
+        commands = rrsettings.get('commands')
+        self.names = []
+        for command in commands:
+            self.names.append(command)
+        if len(self.names) > 0:
+            self.window.show_quick_panel(self.names, self.replace)
+
+
+class RegReplaceCommand(sublime_plugin.TextCommand):
+    def run(self, edit, reg_command=None):
+        view = self.view
+
+        # Did we get a reg command sequence name?
+        if reg_command != None:
+            commands = rrsettings.get('commands')
+
+            # Does reg command sequence exist?
+            if reg_command in commands:
+                reg_pattern = commands[reg_command]
+
+                # Walk through sequence
+                for pattern in reg_pattern:
+                    offset = 0
+                    find = pattern['find']
+                    replace = pattern['replace']
+                    greedy = bool(pattern['greedy'])
+                    case = bool(pattern['case'])
+                    regions = []
+                    flags = 0
+
+                    # Ignore Case?
+                    if not case:
+                        flags |= sublime.IGNORECASE
+
+                    # Greedy or non-greedy search?
+                    if greedy:
+                        regions = view.find_all(find, flags)
+                    else:
+                        region = view.find(find, flags)
+                        if region != None:
+                            regions = [region]
+
+                    # Replace and account for offset after replace
+                    for region in regions:
+                        orig_size = region.size()
+                        new_size = len(replace)
+                        diff = new_size - orig_size
+                        new_region = sublime.Region(region.begin() + offset, region.end() + offset)
+                        view.replace(edit, new_region, replace)
+                        offset += diff
